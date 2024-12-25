ec86c16c51f8158950b2ea22ffbfa25a61243839
Rabinchandra <47386147+Rabinchandra@users.noreply.github.com>
2024-12-25 12:54:47 +0530

    Added - Popular Playlist

---
+++ frontend/model/model.d.ts
@@ -0,0 +1,15 @@
+export interface Song {
+  title: string;
+  artist: string;
+  category: string;
+  url: string;
+  thumbnailUrl: string;
+  duration: string; // Consider using a more specific type like 'Duration'
+}
+
+export interface Playlist {
+  title: string;
+  caption: string;
+  thumbnail: string;
+  songs: Song[];
+}
